effd01027a6f7ad385bfca7caef114759061df92
Tymon Thi <t.thi@f5.com>
2023-12-16 13:41:48 -0800

    feat: add conventional commit support

--- lua/plugins/core/cmp.lua
+++ lua/plugins/core/cmp.lua
@@ -3,30 +3,49 @@
 local cmp = require "cmp"
 
 return {
-  "hrsh7th/nvim-cmp",
-  opts = {
-    mapping = cmp.mapping.preset.insert {
-      ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-      ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-      ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-      ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ["<S-CR>"] = cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-      }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ["<C-CR>"] = function(fallback)
-        cmp.abort()
-        fallback()
-      end,
+  {
+    "hrsh7th/nvim-cmp",
+    opts = {
+      mapping = cmp.mapping.preset.insert {
+        -- Add <C-j> and <C-K> keybinds for navigation.
+        ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<S-CR>"] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-CR>"] = function(fallback)
+          cmp.abort()
+          fallback()
+        end,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
     },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
+  },
+  -- Conventional commits completion in cmp
+  {
+    "davidsierradz/cmp-conventionalcommits",
+    ft = { "gitcommit" },
+    dependencies = "hrsh7th/nvim-cmp",
+    config = function()
+      local ok, _ = pcall(require, "cmp-conventionalcommits")
+      if not ok then
+        return
+      end
+
+      cmp.setup.buffer {
+        sources = require("cmp").config.sources({ { name = "conventionalcommits" } }, { { name = "buffer" } }),
+      }
+    end,
   },
 }
