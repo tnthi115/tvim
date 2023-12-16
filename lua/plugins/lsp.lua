96aede9e5a76b241678dc0ce994b7f67f65fa79e
Tymon Thi <t.thi@f5.com>
2023-12-16 15:15:48 -0800

    fix: lsp diagnostic borders

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,9 +22,9 @@ return {
         -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
         -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
         -- prefix = "icons",
-        float = {
-          border = "rounded",
-        },
+      },
+      float = {
+        border = "rounded",
       },
       severity_sort = true,
     },
