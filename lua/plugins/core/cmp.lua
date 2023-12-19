1b7eea4217f90470724aa89628a677ac9a142956
Tymon Thi <t.thi@f5.com>
2023-12-18 18:38:44 -0800

    feat(core.cmp): simplify <Tab> and <S-Tab> keybinds, change abort keybind to <C-a> to avoid collision with tmux prefix, and remove comments

--- lua/plugins/core/cmp.lua
+++ lua/plugins/core/cmp.lua
@@ -1,160 +1,53 @@
 -- Full spec: https://www.lazyvim.org/plugins/coding#nvim-cmp
 
--- local M = {}
--- M.methods = {}
-
 local cmp = require "cmp"
-local luasnip = require "luasnip"
-local cmp_mapping = require "cmp.config.mapping"
-
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-end
--- M.methods.has_words_before = has_words_before
---
----when inside a snippet, seeks to the nearest luasnip field if possible, and checks if it is jumpable
----@param dir number 1 for forward, -1 for backward; defaults to 1
----@return boolean true if a jumpable luasnip field is found while inside a snippet
-local function jumpable(dir)
-  -- local luasnip_ok, luasnip = pcall(require, "luasnip")
-  -- if not luasnip_ok then
-  --   return false
-  -- end
-
-  local win_get_cursor = vim.api.nvim_win_get_cursor
-  local get_current_buf = vim.api.nvim_get_current_buf
-
-  ---sets the current buffer's luasnip to the one nearest the cursor
-  ---@return boolean true if a node is found, false otherwise
-  local function seek_luasnip_cursor_node()
-    -- TODO(kylo252): upstream this
-    -- for outdated versions of luasnip
-    if not luasnip.session.current_nodes then
-      return false
-    end
-
-    local node = luasnip.session.current_nodes[get_current_buf()]
-    if not node then
-      return false
-    end
-
-    local snippet = node.parent.snippet
-    local exit_node = snippet.insert_nodes[0]
-
-    local pos = win_get_cursor(0)
-    pos[1] = pos[1] - 1
-
-    -- exit early if we're past the exit node
-    if exit_node then
-      local exit_pos_end = exit_node.mark:pos_end()
-      if (pos[1] > exit_pos_end[1]) or (pos[1] == exit_pos_end[1] and pos[2] > exit_pos_end[2]) then
-        snippet:remove_from_jumplist()
-        luasnip.session.current_nodes[get_current_buf()] = nil
-
-        return false
-      end
-    end
-
-    node = snippet.inner_first:jump_into(1, true)
-    while node ~= nil and node.next ~= nil and node ~= snippet do
-      local n_next = node.next
-      local next_pos = n_next and n_next.mark:pos_begin()
-      local candidate = n_next ~= snippet and next_pos and (pos[1] < next_pos[1])
-        or (pos[1] == next_pos[1] and pos[2] < next_pos[2])
-
-      -- Past unmarked exit node, exit early
-      if n_next == nil or n_next == snippet.next then
-        snippet:remove_from_jumplist()
-        luasnip.session.current_nodes[get_current_buf()] = nil
-
-        return false
-      end
-
-      if candidate then
-        luasnip.session.current_nodes[get_current_buf()] = node
-        return true
-      end
-
-      local ok
-      ok, node = pcall(node.jump_from, node, 1, true) -- no_move until last stop
-      if not ok then
-        snippet:remove_from_jumplist()
-        luasnip.session.current_nodes[get_current_buf()] = nil
-
-        return false
-      end
-    end
-
-    -- No candidate, but have an exit node
-    if exit_node then
-      -- to jump to the exit node, seek to snippet
-      luasnip.session.current_nodes[get_current_buf()] = snippet
-      return true
-    end
-
-    -- No exit node, exit from snippet
-    snippet:remove_from_jumplist()
-    luasnip.session.current_nodes[get_current_buf()] = nil
-    return false
-  end
-
-  if dir == -1 then
-    return luasnip.in_snippet() and luasnip.jumpable(-1)
-  else
-    return luasnip.in_snippet() and seek_luasnip_cursor_node() and luasnip.jumpable(1)
-  end
-end
-
--- M.methods.jumpable = jumpable
 
 return {
   {
     "hrsh7th/nvim-cmp",
     opts = {
+      -- mapping = cmp.mapping.preset.insert {
       mapping = cmp.mapping.preset.insert {
         -- Add <C-j> and <C-K> keybinds for navigation.
         ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
         ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
         -- Add <Tab> and <S-Tab> keybinds for navigation.
-        ["<Tab>"] = cmp_mapping(function(fallback)
-          if cmp.visible() then
+        -- https://github.com/hrsh7th/nvim-cmp/issues/286
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if vim.fn.pumvisible() == 1 then
+            vim.fn.feedkey("<C-n>", "n")
+          elseif cmp.visible() then
             cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          elseif jumpable(1) then
-            luasnip.jump(1)
-          elseif has_words_before() then
-            -- cmp.complete()
-            fallback()
           else
             fallback()
           end
         end, { "i", "s" }),
-        ["<S-Tab>"] = cmp_mapping(function(fallback)
-          if cmp.visible() then
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if vim.fn.pumvisible() == 1 then
+            vim.fn.feedkey("<C-p>", "n")
+          elseif cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
           else
             fallback()
           end
         end, { "i", "s" }),
-        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<S-CR>"] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<C-CR>"] = function(fallback)
-          cmp.abort()
-          fallback()
-        end,
+        -- ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        -- ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        -- ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-a>"] = cmp.mapping.abort(),
+        -- ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- ["<S-CR>"] = cmp.mapping.confirm {
+        --   behavior = cmp.ConfirmBehavior.Replace,
+        --   select = true,
+        -- }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- ["<C-CR>"] = function(fallback)
+        --   cmp.abort()
+        --   fallback()
+        -- end,
       },
       window = {
         completion = cmp.config.window.bordered(),
@@ -163,18 +56,14 @@ return {
     },
   },
   -- Conventional commits completion in cmp
+  -- TODO: add commitlint
   {
     "davidsierradz/cmp-conventionalcommits",
     ft = { "gitcommit" },
     dependencies = "hrsh7th/nvim-cmp",
     config = function()
-      local ok, _ = pcall(require, "cmp-conventionalcommits")
-      if not ok then
-        return
-      end
-
       cmp.setup.buffer {
-        sources = require("cmp").config.sources({ { name = "conventionalcommits" } }, { { name = "buffer" } }),
+        sources = cmp.config.sources({ { name = "conventionalcommits" } }, { { name = "buffer" } }),
       }
     end,
   },
