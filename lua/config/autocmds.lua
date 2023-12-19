84579da204fb77d27474f054a4c2b6042e0d2e2e
Tymon Thi <t.thi@f5.com>
2023-12-18 18:41:23 -0800

    feat(autocmds): add autocmd for codelens refresh from lunarvim, add keybind for codelens action

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -15,3 +15,45 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     vim.keymap.set("n", "<leader>M", "<cmd>!firefox %<CR>", { desc = "HTML Preview" })
   end,
 })
+
+local lsp_cmds = vim.api.nvim_create_augroup("lsp_cmds", { clear = true })
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = lsp_cmds,
+  desc = "Code Lens Refresh",
+  callback = function(event)
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    local bufnr = event.buf
+
+    local status_ok, codelens_supported = pcall(function()
+      return client.supports_method "textDocument/codeLens"
+    end)
+    if not status_ok or not codelens_supported then
+      return
+    end
+    local group = "lsp_code_lens_refresh"
+    local cl_events = { "BufEnter", "InsertLeave" }
+    local ok, cl_autocmds = pcall(vim.api.nvim_get_autocmds, {
+      group = group,
+      buffer = bufnr,
+      event = cl_events,
+    })
+
+    if ok and #cl_autocmds > 0 then
+      return
+    end
+    vim.api.nvim_create_augroup(group, { clear = false })
+    vim.api.nvim_create_autocmd(cl_events, {
+      group = group,
+      buffer = bufnr,
+      callback = vim.lsp.codelens.refresh,
+    })
+    vim.lsp.codelens.refresh()
+  end,
+})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = lsp_cmds,
+  desc = "Code Lens Action Keybind",
+  callback = function()
+    vim.keymap.set("n", "<leader>cL", "<cmd>lua vim.lsp.codelens.run()<cr>", { desc = "CodeLens Action" })
+  end,
+})
