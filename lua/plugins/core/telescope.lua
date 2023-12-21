1a7b270a27ad4301e9d9e7f8a171370befb17981
Tymon Thi <tnthi115@berkeley.edu>
2023-12-21 13:55:02 -0800

    feat(extras.project): disable manual_mode and use "pattern" detection method first for automatic project adding

--- lua/plugins/core/telescope.lua
+++ lua/plugins/core/telescope.lua
@@ -70,6 +70,15 @@ return {
   {
     "ahmedkhalf/project.nvim",
     opts = {
+      -- Manual mode doesn't automatically change your root directory, so you have
+      -- the option to manually do so using `:ProjectRoot` command.
+      manual_mode = false,
+      -- Methods of detecting the root directory. **"lsp"** uses the native neovim
+      -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
+      -- order matters: if one is not detected, the other is used as fallback. You
+      -- can also delete or rearangne the detection methods.
+      detection_methods = { "pattern", "lsp" },
+      -- detection_methods = { "pattern" },
       show_hidden = true,
     },
   },
