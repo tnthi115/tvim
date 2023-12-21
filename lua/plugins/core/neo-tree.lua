0ff32a693d0c444e8d06958f045192dc4fd0d4e7
Tymon Thi <tnthi115@berkeley.edu>
2023-12-21 13:54:58 -0800

    feat(core.neo-tree): disable netrw plugin with lazy.nvim and let neo-tree hijack

--- lua/plugins/core/neo-tree.lua
+++ lua/plugins/core/neo-tree.lua
@@ -14,6 +14,17 @@ return {
       filtered_items = {
         visible = true,
       },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+      -- "open_default" -- netrw disabled, opening a directory opens neo-tree
+      --                -- in whatever position is specified in window.position
+      -- "open_current" -- netrw disabled, opening a directory opens within the
+      --                -- window like netrw would, regardless of window.position
+      -- "disabled"     -- netrw left alone, neo-tree does not handle opening dirs
+      hijack_netrw_behavior = "open_current",
     },
   },
 }
