62bf3848bcef62a231c432321a9805c54b2a4152
Tymon Thi <tnthi115@berkeley.edu>
2023-12-27 11:37:04 -0800

    feat(hardtime.nvim): add hardtime.nvim to address bad habits

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,8 @@
+-- https://github.com/m4xshen/hardtime.nvim
+
+return {
+  "m4xshen/hardtime.nvim",
+  event = "VeryLazy",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  opts = {},
+}
