e5526030f99e440e3b85319a28921fa62eb12be7
Tymon Thi <tnthi115@berkeley.edu>
2023-12-27 17:01:46 -0800

    feat(hardtime): enable mouse and add keybind for :Hardtime toggle

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,8 +1,12 @@
 -- https://github.com/m4xshen/hardtime.nvim
+-- https://github.com/m4xshen/hardtime.nvim/issues/5
 
 return {
   "m4xshen/hardtime.nvim",
   event = "VeryLazy",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {},
+  keys = { { "<leader>uH", "<cmd>Hardtime toggle<CR>", desc = "Toggle Hardtime" } },
+  opts = {
+    disable_mouse = false,
+  },
 }
