e97bb7b222dd3c0e62b936ffc1279dc44e550095
Tymon Thi <tnthi115@berkeley.edu>
2023-12-29 00:55:12 -0800

    refactor(gitlinker): add whichkey default menu name and add keys with lazy.nvim

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -1,36 +1,24 @@
 -- https://github.com/linrongbin16/gitlinker.nvim
 
 return {
-  "linrongbin16/gitlinker.nvim",
-  config = function()
-    require("gitlinker").setup()
-
-    local which_key_ok, which_key = pcall(require, "which-key")
-    if not which_key_ok then
-      return
-    end
-
-    local opts = {
-      mode = { "n", "v" }, -- NORMAL and VISUAL mode
-      prefix = "<leader>",
-      buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-      silent = true, -- use `silent` when creating keymaps
-      noremap = true, -- use `noremap` when creating keymaps
-      nowait = true, -- use `nowait` when creating keymaps
-    }
-
-    local mappings = {
-      g = {
-        L = {
-          name = "+gitlinker",
-          l = { "<cmd>GitLink<CR>", "Copy /blob URL to Clipboard" },
-          L = { "<cmd>GitLink!<CR>", "Open /blob URL in Browser" },
-          b = { "<cmd>GitLink blame<CR>", "Copy /blame URL to Clipboard" },
-          B = { "<cmd>GitLink! blame<CR>", "Open /blame URL in Browser" },
-        },
-      },
-    }
-
-    which_key.register(mappings, opts)
-  end,
+  {
+    "folke/which-key.nvim",
+    opts = function(_, opts)
+      opts.window = {
+        border = "single",
+      }
+      opts.defaults["<leader>gL"] = { name = "+gitlinker" }
+    end,
+  },
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    keys = {
+      { mode = { "n", "v" }, "<leader>gLl", "<cmd>GitLink<CR>", desc = "Copy /blob URL to Clipboard" },
+      { mode = { "n", "v" }, "<leader>gLL", "<cmd>GitLink!<CR>", desc = "Open /blob URL in Browser" },
+      { mode = { "n", "v" }, "<leader>gLb", "<cmd>GitLink blame<CR>", desc = "Copy /blame URL to Clipboard" },
+      { mode = { "n", "v" }, "<leader>gLB", "<cmd>GitLink! blame<CR>", desc = "Open /blame URL in Browser" },
+    },
+    opts = {},
+  },
 }
