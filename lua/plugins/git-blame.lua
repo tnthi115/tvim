f32a05c4eceef114300025809fdbfc452c5d20a5
Tymon Thi <tnthi115@berkeley.edu>
2023-12-29 00:54:24 -0800

    feat(git-blame): add git-blame.nvim for inline virtual text git blame info like I saw in vscode

---
+++ lua/plugins/git-blame.lua
@@ -0,0 +1,13 @@
+-- https://github.com/f-person/git-blame.nvim
+
+return {
+  "f-person/git-blame.nvim",
+  cmd = { "GitBlameToggle", "GitBlameEnable", "GitBlameDisable" },
+  keys = {
+    { "<leader>ug", "<cmd>GitBlameToggle<CR>", desc = "Toggle Git Blame Virtual Text" },
+    { "<leader>gB", "<cmd>GitBlameEnable<CR>", desc = "Enable Git Blame Virtual Text" },
+  },
+  opts = {
+    enabled = false,
+  },
+}
