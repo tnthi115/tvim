7ea9c08718b0ce28103ff87195e40b0d4030a312
Tymon Thi <tnthi115@berkeley.edu>
2023-12-29 17:15:34 -0800

    fix(barbecue): remove navic config

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -6,29 +6,30 @@ return {
   name = "barbecue",
   version = "*",
   dependencies = {
-    {
-      "SmiteshP/nvim-navic",
-      init = function()
-        vim.g.navic_silence = true
-        require("lazyvim.util").lsp.on_attach(function(client, buffer)
-          if client.supports_method "textDocument/documentSymbol" then
-            require("nvim-navic").attach(client, buffer)
-          end
-        end)
-      end,
-      opts = function()
-        return {
-          separator = " ",
-          highlight = true,
-          depth_limit = 5,
-          icons = require("lazyvim.config").icons.kinds,
-          lazy_update_context = true,
-          lsp = {
-            auto_attach = true,
-          },
-        }
-      end,
-    },
+    -- {
+    --   "SmiteshP/nvim-navic",
+    --   init = function()
+    --     vim.g.navic_silence = true
+    --     require("lazyvim.util").lsp.on_attach(function(client, buffer)
+    --       if client.supports_method "textDocument/documentSymbol" then
+    --         require("nvim-navic").attach(client, buffer)
+    --       end
+    --     end)
+    --   end,
+    --   opts = function()
+    --     return {
+    --       separator = " ",
+    --       highlight = true,
+    --       -- depth_limit = 5,
+    --       icons = require("lazyvim.config").icons.kinds,
+    --       -- lazy_update_context = true,
+    --       lsp = {
+    --         auto_attach = true,
+    --       },
+    --     }
+    --   end,
+    -- },
+    "SmiteshP/nvim-navic",
     "nvim-tree/nvim-web-devicons",
   },
   opts = {
@@ -60,6 +61,7 @@ return {
     --   Operator = "",
     --   TypeParameter = "",
     -- },
+    -- kinds = require("lazyvim.config").icons.kinds,
     kinds = {
       Array = "",
       Boolean = "󰨙",
