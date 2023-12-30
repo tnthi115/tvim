c9f63b94ef95bc179ead94b920ca93e582313517
Tymon Thi <tnthi115@berkeley.edu>
2023-12-29 17:15:49 -0800

    feat(leetcode): add whichkey mappings

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -14,77 +14,121 @@ return {
     "rcarriga/nvim-notify",
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {
-    ---@type string
-    arg = "leetcode.nvim",
+  opts = function()
+    local which_key_ok, which_key = pcall(require, "which-key")
+    if which_key_ok then
+      local opts = {
+        mode = "n", -- NORMAL
+        prefix = "<leader>",
+        buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+        silent = true, -- use `silent` when creating keymaps
+        noremap = true, -- use `noremap` when creating keymaps
+        nowait = true, -- use `nowait` when creating keymaps
+      }
 
-    ---@type lc.lang
-    lang = "golang",
+      local mappings = {
+        L = {
+          name = "+leetcode",
+          m = { "<cmd>Leet menu<CR>", "Menu" },
+          c = { "<cmd>Leet console<CR>", "Console" },
+          i = { "<cmd>Leet info<CR>", "Info" },
+          [","] = { "<cmd>Leet tabs<CR>", "Tabs" },
+          L = { "<cmd>Leet lang<CR>", "Lang" },
+          r = { "<cmd>Leet run<CR>", "Run" },
+          t = { "<cmd>Leet test<CR>", "Test" },
+          s = { "<cmd>Leet submit<CR>", "Submit" },
+          R = { "<cmd>Leet random<CR>", "Random" },
+          D = { "<cmd>Leet daily<CR>", "Daily" },
+          l = { "<cmd>Leet list<CR>", "List" },
+          d = {
+            name = "Desc",
+            d = { "<cmd>Leet desc toggle<CR>", "Toggle" },
+            s = { "<cmd>Leet desc stats<CR>", "Stats" },
+          },
+          C = {
+            name = "Cookie",
+            d = { "<cmd>Leet cookie update<CR>", "Update" },
+            s = { "<cmd>Leet cookie delete<CR>", "Delete (Sign-out)" },
+          },
+          u = { "<cmd>Leet cache update<CR>", "Update Cache" },
+        },
+      }
 
-    cn = { -- leetcode.cn
-      enabled = false, ---@type boolean
-      translator = true, ---@type boolean
-      translate_problems = true, ---@type boolean
-    },
+      which_key.register(mappings, opts)
+    end
 
-    ---@type string
-    directory = vim.fn.stdpath "data" .. "/leetcode/",
+    return {
+      ---@type string
+      arg = "leetcode.nvim",
 
-    ---@type boolean
-    logging = true,
+      ---@type lc.lang
+      lang = "golang",
 
-    cache = {
-      update_interval = 60 * 60 * 24 * 7, ---@type integer 7 days
-    },
-
-    console = {
-      open_on_runcode = true, ---@type boolean
-
-      dir = "row", ---@type lc.direction
-
-      size = { ---@type lc.size
-        width = "90%",
-        height = "75%",
+      cn = { -- leetcode.cn
+        enabled = false, ---@type boolean
+        translator = true, ---@type boolean
+        translate_problems = true, ---@type boolean
       },
 
-      result = {
-        size = "60%", ---@type lc.size
+      ---@type string
+      directory = vim.fn.stdpath "data" .. "/leetcode/",
+
+      ---@type boolean
+      logging = true,
+
+      cache = {
+        update_interval = 60 * 60 * 24 * 7, ---@type integer 7 days
       },
 
-      testcase = {
-        virt_text = true, ---@type boolean
+      console = {
+        open_on_runcode = true, ---@type boolean
 
-        size = "40%", ---@type lc.size
+        dir = "row", ---@type lc.direction
+
+        size = { ---@type lc.size
+          width = "90%",
+          height = "75%",
+        },
+
+        result = {
+          size = "60%", ---@type lc.size
+        },
+
+        testcase = {
+          virt_text = true, ---@type boolean
+
+          size = "40%", ---@type lc.size
+        },
       },
-    },
 
-    description = {
-      position = "left", ---@type lc.position
+      description = {
+        position = "left", ---@type lc.position
 
-      width = "40%", ---@type lc.size
+        width = "40%", ---@type lc.size
 
-      show_stats = true, ---@type boolean
-    },
+        show_stats = true, ---@type boolean
+      },
 
-    hooks = {
-      ---@type fun()[]
-      LeetEnter = {},
+      hooks = {
+        ---@type fun()[]
+        LeetEnter = {},
 
-      ---@type fun(question: { lang: string })[]
-      LeetQuestionNew = {},
-    },
+        ---@type fun(question: { lang: string })[]
+        LeetQuestionNew = {},
+      },
 
-    ---@type boolean
-    image_support = false, -- setting this to `true` will disable question description wrap
+      ---@type boolean
+      image_support = true, -- setting this to `true` will disable question description wrap
 
-    keys = {
-      toggle = { "q", "<Esc>" }, ---@type string|string[]
-      confirm = { "<CR>" }, ---@type string|string[]
+      keys = {
+        toggle = { "q", "<Esc>" }, ---@type string|string[]
+        confirm = { "<CR>" }, ---@type string|string[]
 
-      reset_testcases = "r", ---@type string
-      use_testcase = "U", ---@type string
-      focus_testcases = "H", ---@type string
-      focus_result = "L", ---@type string
-    },
-  },
+        reset_testcases = "r", ---@type string
+        use_testcase = "U", ---@type string
+        focus_testcases = "H", ---@type string
+        focus_result = "L", ---@type string
+      },
+    }
+  end,
 }
