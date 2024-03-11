4e03fab50d82b88824b85d827af1450609c7a48c
Tymon Thi <tnthi115@berkeley.edu>
2024-03-11 12:00:00 -0700

    feat(gitlinker): update and add keybinds for default_branch and current_branch

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -16,6 +16,45 @@ return {
     { mode = { "n", "v" }, "<leader>gLL", "<cmd>GitLink!<CR>", desc = "Open /blob URL in Browser" },
     { mode = { "n", "v" }, "<leader>gLb", "<cmd>GitLink blame<CR>", desc = "Copy /blame URL to Clipboard" },
     { mode = { "n", "v" }, "<leader>gLB", "<cmd>GitLink! blame<CR>", desc = "Open /blame URL in Browser" },
+    { mode = { "n", "v" }, "<leader>gLB", "<cmd>GitLink! blame<CR>", desc = "Open /blame URL in Browser" },
+    {
+      mode = { "n", "v" },
+      "<leader>gLd",
+      function()
+        require("gitlinker").link { router_type = "default_branch" }
+      end,
+      desc = "GitLink default_branch",
+    },
+    {
+      mode = { "n", "v" },
+      "<leader>gLD",
+      function()
+        require("gitlinker").link {
+          router_type = "default_branch",
+          action = require("gitlinker.actions").system,
+        }
+      end,
+      desc = "GitLink! default_branch",
+    },
+    {
+      mode = { "n", "v" },
+      "<leader>gLc",
+      function()
+        require("gitlinker").link { router_type = "current_branch" }
+      end,
+      desc = "GitLink current_branch",
+    },
+    {
+      mode = { "n", "v" },
+      "<leader>gLC",
+      function()
+        require("gitlinker").link {
+          router_type = "current_branch",
+          action = require("gitlinker.actions").system,
+        }
+      end,
+      desc = "GitLink! current_branch",
+    },
   },
   opts = {},
 }
