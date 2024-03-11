1fe19f6dcf22cca6486bbaefd16303dccbe80b88
Tymon Thi <tnthi115@berkeley.edu>
2024-03-11 12:22:21 -0700

    feat(obsidian): update obsidian.nvim configuration after updates

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -68,6 +68,8 @@ return {
               f = { "<cmd>ObsidianFollowLink<CR>", "Follow Link" },
               b = { "<cmd>ObsidianBacklinks<CR>", "Backlinks" },
               d = { "<cmd>ObsidianToday<CR>", "Today's Daily note" },
+              y = { "<cmd>ObsidianYesterday<CR>", "Yesterday's Daily note" },
+              T = { "<cmd>ObsidianTomorrow<CR>", "Tomorrow's Daily note" },
               t = { "<cmd>ObsidianTemplate<CR>", "Template" },
               ["/"] = { "<cmd>ObsidianSearch<CR>", "Search" },
               l = { "<cmd>ObsidianLink<CR>", "Link" },
@@ -103,10 +105,6 @@ return {
         -- 'workspaces'. For example:
         -- dir = "~/vaults/work",
 
-        -- Optional, set to true to use the current directory as a vault; otherwise
-        -- the first workspace is opened by default.
-        detect_cwd = false,
-
         -- Optional, if you keep notes in a specific subdirectory of your vault.
         notes_subdir = "notes",
 
@@ -132,26 +130,6 @@ return {
 
           -- Trigger completion at 2 chars.
           min_chars = 1,
-
-          -- Where to put new notes created from completion. Valid options are
-          --  * "current_dir" - put new notes in same directory as the current buffer.
-          --  * "notes_subdir" - put new notes in the default notes subdirectory.
-          new_notes_location = "current_dir",
-
-          -- Control how wiki links are completed with these (mutually exclusive) options:
-          --
-          -- 1. Whether to add the note ID during completion.
-          -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
-          -- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
-          prepend_note_id = true,
-          -- 2. Whether to add the note path during completion.
-          -- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
-          -- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
-          prepend_note_path = false,
-          -- 3. Whether to only use paths during completion.
-          -- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
-          -- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
-          use_path_only = false,
         },
 
         -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
@@ -174,6 +152,11 @@ return {
           },
         },
 
+        -- Where to put new notes. Valid options are
+        --  * "current_dir" - put new notes in same directory as the current buffer.
+        --  * "notes_subdir" - put new notes in the default notes subdirectory.
+        new_notes_location = "notes_subdir",
+
         -- Optional, customize how names/IDs for new notes are created.
         note_id_func = function(title)
           -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
@@ -192,19 +175,54 @@ return {
           return tostring(os.time()) .. "-" .. suffix
         end,
 
+        -- Optional, customize how note file names are generated given the ID, target directory, and title.
+        ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+        ---@return string|obsidian.Path The full path to the new note.
+        note_path_func = function(spec)
+          -- This is equivalent to the default behavior.
+          local path = spec.dir / tostring(spec.id)
+          return path:with_suffix ".md"
+        end,
+
+        -- Optional, customize how wiki links are formatted. You can set this to one of:
+        --  * "use_alias_only", e.g. '[[Foo Bar]]'
+        --  * "prepend_note_id", e.g. '[[foo-bar|Foo Bar]]'
+        --  * "prepend_note_path", e.g. '[[foo-bar.md|Foo Bar]]'
+        --  * "use_path_only", e.g. '[[foo-bar.md]]'
+        -- Or you can set it to a function that takes a table of options and returns a string, like this:
+        wiki_link_func = function(opts)
+          return require("obsidian.util").wiki_link_id_prefix(opts)
+        end,
+
+        -- Optional, customize how markdown links are formatted.
+        markdown_link_func = function(opts)
+          return require("obsidian.util").markdown_link(opts)
+        end,
+
+        -- Either 'wiki' or 'markdown'.
+        preferred_link_style = "wiki",
+
+        -- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
+        ---@return string
+        image_name_func = function()
+          -- Prefix image names with timestamp.
+          return string.format("%s-", os.time())
+        end,
+
         -- Optional, boolean or a function that takes a filename and returns a boolean.
         -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
-        disable_frontmatter = true,
+        disable_frontmatter = false,
 
         -- Optional, alternatively you can customize the frontmatter data.
+        ---@return table
         note_frontmatter_func = function(note)
-          -- This is equivalent to the default frontmatter function.
-          -- local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-          local out = {
-            aliases = note.aliases,
-            created = note.created,
-            modified = note.modified,
-          }
+          -- Add the title of the note as an alias.
+          if note.title then
+            note:add_alias(note.title)
+          end
+
+          local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
           -- `note.metadata` contains any manually added fields in the frontmatter.
           -- So here we just make sure those fields are kept in the frontmatter.
           if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
@@ -212,6 +230,7 @@ return {
               out[k] = v
             end
           end
+
           return out
         end,
 
@@ -222,14 +241,6 @@ return {
           time_format = "%H:%M",
           -- A map for custom variables, the key should be the variable and the value a function
           substitutions = {},
-        },
-
-        -- Optional, customize the backlinks interface.
-        backlinks = {
-          -- The default height of the backlinks pane.
-          height = 10,
-          -- Whether or not to wrap lines.
-          wrap = true,
         },
 
         -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
@@ -247,11 +258,18 @@ return {
         -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
         open_app_foreground = false,
 
-        -- Optional, by default commands like `:ObsidianSearch` will attempt to use
-        -- telescope.nvim, fzf-lua, fzf.vim, or mini.pick (in that order), and use the
-        -- first one they find. You can set this option to tell obsidian.nvim to always use this
-        -- finder.
-        finder = "telescope.nvim",
+        picker = {
+          -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+          name = "telescope.nvim",
+          -- Optional, configure key mappings for the picker. These are the defaults.
+          -- Not all pickers support all mappings.
+          mappings = {
+            -- Create a new note from your query.
+            new = "<C-x>",
+            -- Insert a link to the selected note.
+            insert_link = "<C-l>",
+          },
+        },
 
         -- Optional, sort search results by "path", "modified", "accessed", or "created".
         -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
@@ -265,7 +283,35 @@ return {
         -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
         open_notes_in = "current",
 
+        -- Optional, define your own callbacks to further customize behavior.
+        callbacks = {
+          -- Runs at the end of `require("obsidian").setup()`.
+          ---@param client obsidian.Client
+          post_setup = function(client) end,
+
+          -- Runs anytime you enter the buffer for a note.
+          ---@param client obsidian.Client
+          ---@param note obsidian.Note
+          enter_note = function(client, note) end,
+
+          -- Runs anytime you leave the buffer for a note.
+          ---@param client obsidian.Client
+          ---@param note obsidian.Note
+          leave_note = function(client, note) end,
+
+          -- Runs right before writing the buffer for a note.
+          ---@param client obsidian.Client
+          ---@param note obsidian.Note
+          pre_write_note = function(client, note) end,
+
+          -- Runs anytime the workspace is set/changed.
+          ---@param client obsidian.Client
+          ---@param workspace obsidian.Workspace
+          post_set_workspace = function(client, workspace) end,
+        },
+
         -- Optional, configure additional syntax highlighting / extmarks.
+        -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
         ui = {
           enable = true, -- set to false to disable all additional syntax features
           update_debounce = 200, -- update delay after a text change (in milliseconds)
@@ -282,21 +328,26 @@ return {
 
             -- You can also add more custom ones...
           },
+          -- Use bullet marks for non-checkbox lists.
+          bullets = { char = "•", hl_group = "ObsidianBullet" },
           external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
           -- Replace the above with this if you don't have a patched font:
           -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
           reference_text = { hl_group = "ObsidianRefText" },
           highlight_text = { hl_group = "ObsidianHighlightText" },
           tags = { hl_group = "ObsidianTag" },
+          block_ids = { hl_group = "ObsidianBlockID" },
           hl_groups = {
             -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
             ObsidianTodo = { bold = true, fg = "#f78c6c" },
             ObsidianDone = { bold = true, fg = "#89ddff" },
             ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
             ObsidianTilde = { bold = true, fg = "#ff5370" },
+            ObsidianBullet = { bold = true, fg = "#89ddff" },
             ObsidianRefText = { underline = true, fg = "#c792ea" },
             ObsidianExtLinkIcon = { fg = "#c792ea" },
             ObsidianTag = { italic = true, fg = "#89ddff" },
+            ObsidianBlockID = { italic = true, fg = "#89ddff" },
             ObsidianHighlightText = { bg = "#75662e" },
           },
         },
@@ -328,14 +379,6 @@ return {
             return string.format("![%s](%s)", display_name, link_path)
           end,
         },
-
-        -- Optional, set the YAML parser to use. The valid options are:
-        --  * "native" - uses a pure Lua parser that's fast but potentially misses some edge cases.
-        --  * "yq" - uses the command-line tool yq (https://github.com/mikefarah/yq), which is more robust
-        --    but much slower and needs to be installed separately.
-        -- In general you should be using the native parser unless you run into a bug with it, in which
-        -- case you can temporarily switch to the "yq" parser until the bug is fixed.
-        yaml_parser = "native",
       }
     end,
   },
