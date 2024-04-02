037f28dfc9e4996b58bfe44afa28bcc43e284786
Tymon Thi <tnthi115@berkeley.edu>
2024-04-02 12:33:47 -0700

    refactor(flash): reorder config options

--- lua/plugins/core/flash.lua
+++ lua/plugins/core/flash.lua
@@ -3,16 +3,6 @@
 -- flash.nvim - navigate code with search labels, enhanced character motions, and Treesitter integration
 return {
   "folke/flash.nvim",
-  ---@type Flash.Config
-  opts = {
-    modes = {
-      -- `f`, `F`, `t`, `T`, `;` and `,` motions
-      char = {
-        enabled = false,
-        jump_labels = true,
-      },
-    },
-  },
   -- stylua: ignore
   keys = {
     -- disable the default flash keymap
@@ -23,5 +13,15 @@ return {
     -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     -- { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      -- `f`, `F`, `t`, `T`, `;` and `,` motions
+      char = {
+        enabled = false,
+        jump_labels = true,
+      },
+    },
   },
 }
