83f5efe1fcf87ddb9ce499c8535b755a1fb968ce
Tymon Thi <tnthi115@berkeley.edu>
2024-04-04 18:17:35 -0700

    feat(gitsigns): update git blame current line format

--- lua/plugins/core/gitsigns.lua
+++ lua/plugins/core/gitsigns.lua
@@ -33,10 +33,12 @@ return {
     -- Time format reference: https://github.com/f-person/git-blame.nvim?tab=readme-ov-file#date-format
     -- current_line_blame_formatter = "<author>, <author_time:%c> - <summary>",
     -- current_line_blame_formatter = "<author> • <author_time:%a, %d %b %Y %X> • <summary>",
-    current_line_blame_formatter = "<author> • <author_time:%c> • <summary>",
-    current_line_blame_formatter_opts = {
-      relative_time = false,
-    },
+    -- current_line_blame_formatter = "    <author> • <author_time:%c (%Y-%m-%d)> • <summary>",
+    -- current_line_blame_formatter_opts = {
+    --   relative_time = true,
+    -- },
+    current_line_blame_formatter = "    <author> • <author_time:%c> • <summary>",
+    current_line_blame_formatter_nc = "    <author>",
     current_line_blame_opts = {
       delay = 0,
     },
