4edae693208c8eb89beb636c035beb2a83e480fd
Tymon Thi <tnthi115@berkeley.edu>
2024-05-20 10:42:41 -0700

    refactor: remove mini.indentscope config and add indent-blankline config after LazyVim update

---
+++ lua/plugins/core/indent-blankline.lua
@@ -0,0 +1,10 @@
+-- Full spec: https://www.lazyvim.org/plugins/ui#indent-blanklinenvim
+
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  opts = {
+    scope = {
+      highlight = { "Normal" },
+    },
+  },
+}
