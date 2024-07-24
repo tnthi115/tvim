3629da39576899e4e65f073962a2d8f57c312275
Tymon Thi <tnthi115@berkeley.edu>
2024-07-24 10:41:34 -0700

    feat(obsidian): remove ui syntax and only load for vault files after LazyVim markdown update that adds markdown.nvim

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -24,15 +24,15 @@ return {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
     lazy = true,
-    ft = "markdown",
+    -- ft = "markdown",
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    --   -- stylua: ignore
-    --   "BufReadPre " .. vault_path .. "**.md",
-    --   "BufNewFile " .. vault_path .. "**.md",
-    -- },
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      -- stylua: ignore
+      "BufReadPre " .. vault_path .. "**.md",
+      "BufNewFile " .. vault_path .. "**.md",
+    },
     dependencies = {
       -- Required.
       "nvim-lua/plenary.nvim",
@@ -317,7 +317,7 @@ return {
         -- Optional, configure additional syntax highlighting / extmarks.
         -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
         ui = {
-          enable = true, -- set to false to disable all additional syntax features
+          enable = false, -- set to false to disable all additional syntax features
           update_debounce = 200, -- update delay after a text change (in milliseconds)
           -- Define how various check-boxes are displayed
           checkboxes = {
