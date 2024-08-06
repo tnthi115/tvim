41040fd7fea13cdd93753f342c8f043bfb230229
Tymon Thi <tnthi115@berkeley.edu>
2024-08-06 09:55:36 -0700

    feat(lualine): sync some things from lazyvim lualine

--- lua/plugins/core/lualine.lua
+++ lua/plugins/core/lualine.lua
@@ -140,7 +140,7 @@ return {
         },
       }
 
-      return {
+      local opts = {
         options = {
           theme = "auto",
           globalstatus = true,
@@ -251,7 +251,9 @@ return {
             {
               require("lazy.status").updates,
               cond = require("lazy.status").has_updates,
-              color = Util.ui.fg "Special",
+              color = function()
+                return LazyVim.ui.fg "Special"
+              end,
             },
             {
               "diagnostics",
@@ -318,6 +320,26 @@ return {
         },
         extensions = { "neo-tree", "lazy" },
       }
+      -- -- do not add trouble symbols if aerial is enabled
+      -- -- And allow it to be overriden for some buffer types (see autocmds)
+      -- if vim.g.trouble_lualine and LazyVim.has "trouble.nvim" then
+      --   local trouble = require "trouble"
+      --   local symbols = trouble.statusline {
+      --     mode = "symbols",
+      --     groups = {},
+      --     title = false,
+      --     filter = { range = true },
+      --     format = "{kind_icon}{symbol.name:Normal}",
+      --     hl_group = "lualine_c_normal",
+      --   }
+      --   table.insert(opts.sections.lualine_c, {
+      --     symbols and symbols.get,
+      --     cond = function()
+      --       return vim.b.trouble_lualine ~= false and symbols.has()
+      --     end,
+      --   })
+      -- end
+      return opts
     end,
   },
 }
