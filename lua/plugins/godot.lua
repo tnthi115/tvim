137ada4ca615dce93e2ac5d80587934262658625
Tymon Thi <tnthi115@berkeley.edu>
2024-08-08 20:09:10 -0700

    refactor(godot): use omnisharp lazy extra instead of custom config in godot.lua

--- lua/plugins/godot.lua
+++ lua/plugins/godot.lua
@@ -8,103 +8,103 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "c_sharp", "gdscript" })
+      vim.list_extend(opts.ensure_installed, { "gdscript" })
     end,
   },
   -- Install mason packages.
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, {
-        "csharpier",
-      })
-    end,
-  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = opts.ensure_installed or {}
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "csharpier",
+  --     })
+  --   end,
+  -- },
   -- Setup LSPs
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "Hoffs/omnisharp-extended-lsp.nvim",
-    },
+    -- dependencies = {
+    --   "Hoffs/omnisharp-extended-lsp.nvim",
+    -- },
     opts = {
       servers = {
         -- gdscript lsp
         gdscript = {},
         -- c# lsp
-        omnisharp = {
-          -- TODO: these require telescope I believe, and they don't work when using fzf-lua
-          keys = {
-            {
-              "gd",
-              "<cmd>lua require('omnisharp_extended').lsp_definition()<CR>",
-              desc = "Goto Definition (omnisharp)",
-            },
-            {
-              "gy",
-              "<cmd>lua require('omnisharp_extended').lsp_type_definition()<CR>",
-              desc = "Goto T[y]pe Definition (omnisharp)",
-            },
-            { "gr", "<cmd>lua require('omnisharp_extended').lsp_references()<CR>", desc = "References (omnisharp)" },
-            {
-              "gr",
-              "<cmd>lua require('omnisharp_extended').lsp_implementations()<CR>",
-              desc = "Goto Implementation (omnisharp)",
-            },
-          },
-          settings = {
-            -- handlers = {
-            --   ["textDocument/definition"] = function()
-            --     return require("omnisharp_extended").definition_handler
-            --   end,
-            --   ["textDocument/typeDefinition"] = function()
-            --     return require("omnisharp_extended").type_definition_handler
-            --   end,
-            --   ["textDocument/references"] = function()
-            --     return require("omnisharp_extended").references_handler
-            --   end,
-            --   ["textDocument/implementation"] = function()
-            --     return require("omnisharp_extended").implementation_handler
-            --   end,
-            -- },
-            -- cmd = { "dotnet", "/path/to/omnisharp/OmniSharp.dll" },
-
-            -- Enables support for reading code style, naming convention and analyzer
-            -- settings from .editorconfig.
-            enable_editorconfig_support = true,
-
-            -- If true, MSBuild project system will only load projects for files that
-            -- were opened in the editor. This setting is useful for big C# codebases
-            -- and allows for faster initialization of code navigation features only
-            -- for projects that are relevant to code that is being edited. With this
-            -- setting enabled OmniSharp may load fewer projects and may thus display
-            -- incomplete reference lists for symbols.
-            enable_ms_build_load_projects_on_demand = false, -- default false
-
-            -- Enables support for roslyn analyzers, code fixes and rulesets.
-            enable_roslyn_analyzers = true, -- default false
-
-            -- Specifies whether 'using' directives should be grouped and sorted during
-            -- document formatting.
-            organize_imports_on_format = true, -- default false
-
-            -- Enables support for showing unimported types and unimported extension
-            -- methods in completion lists. When committed, the appropriate using
-            -- directive will be added at the top of the current file. This option can
-            -- have a negative impact on initial completion responsiveness,
-            -- particularly for the first few completion sessions after opening a
-            -- solution.
-            enable_import_completion = true, -- default false
-
-            -- Specifies whether to include preview versions of the .NET SDK when
-            -- determining which version to use for project loading.
-            sdk_include_prereleases = true,
-
-            -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-            -- true
-            analyze_open_documents_only = true, -- default false
-          },
-        },
+        -- omnisharp = {
+        --   -- TODO: these require telescope I believe, and they don't work when using fzf-lua
+        --   keys = {
+        --     {
+        --       "gd",
+        --       "<cmd>lua require('omnisharp_extended').lsp_definition()<CR>",
+        --       desc = "Goto Definition (omnisharp)",
+        --     },
+        --     {
+        --       "gy",
+        --       "<cmd>lua require('omnisharp_extended').lsp_type_definition()<CR>",
+        --       desc = "Goto T[y]pe Definition (omnisharp)",
+        --     },
+        --     { "gr", "<cmd>lua require('omnisharp_extended').lsp_references()<CR>", desc = "References (omnisharp)" },
+        --     {
+        --       "gr",
+        --       "<cmd>lua require('omnisharp_extended').lsp_implementations()<CR>",
+        --       desc = "Goto Implementation (omnisharp)",
+        --     },
+        --   },
+        --   settings = {
+        --     -- handlers = {
+        --     --   ["textDocument/definition"] = function()
+        --     --     return require("omnisharp_extended").definition_handler
+        --     --   end,
+        --     --   ["textDocument/typeDefinition"] = function()
+        --     --     return require("omnisharp_extended").type_definition_handler
+        --     --   end,
+        --     --   ["textDocument/references"] = function()
+        --     --     return require("omnisharp_extended").references_handler
+        --     --   end,
+        --     --   ["textDocument/implementation"] = function()
+        --     --     return require("omnisharp_extended").implementation_handler
+        --     --   end,
+        --     -- },
+        --     -- cmd = { "dotnet", "/path/to/omnisharp/OmniSharp.dll" },
+        --
+        --     -- Enables support for reading code style, naming convention and analyzer
+        --     -- settings from .editorconfig.
+        --     enable_editorconfig_support = true,
+        --
+        --     -- If true, MSBuild project system will only load projects for files that
+        --     -- were opened in the editor. This setting is useful for big C# codebases
+        --     -- and allows for faster initialization of code navigation features only
+        --     -- for projects that are relevant to code that is being edited. With this
+        --     -- setting enabled OmniSharp may load fewer projects and may thus display
+        --     -- incomplete reference lists for symbols.
+        --     enable_ms_build_load_projects_on_demand = false, -- default false
+        --
+        --     -- Enables support for roslyn analyzers, code fixes and rulesets.
+        --     enable_roslyn_analyzers = true, -- default false
+        --
+        --     -- Specifies whether 'using' directives should be grouped and sorted during
+        --     -- document formatting.
+        --     organize_imports_on_format = true, -- default false
+        --
+        --     -- Enables support for showing unimported types and unimported extension
+        --     -- methods in completion lists. When committed, the appropriate using
+        --     -- directive will be added at the top of the current file. This option can
+        --     -- have a negative impact on initial completion responsiveness,
+        --     -- particularly for the first few completion sessions after opening a
+        --     -- solution.
+        --     enable_import_completion = true, -- default false
+        --
+        --     -- Specifies whether to include preview versions of the .NET SDK when
+        --     -- determining which version to use for project loading.
+        --     sdk_include_prereleases = true,
+        --
+        --     -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+        --     -- true
+        --     analyze_open_documents_only = true, -- default false
+        --   },
+        -- },
       },
     },
   },
