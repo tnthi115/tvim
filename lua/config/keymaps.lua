48174513e9693fcdc072f17e7d482b61b0f99f76
Tymon Thi <tnthi115@berkeley.edu>
2024-08-12 11:35:03 -0700

    refactor(keymaps): remove next and prev diagnostics keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -63,8 +63,8 @@ end, { desc = "Live grep with glob pattern" })
 
 -- Set keymaps for diagnostics like Lunarvim.
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
-vim.keymap.set("n", "<leader>cj", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
-vim.keymap.set("n", "<leader>ck", vim.diagnostic.goto_prev, { desc = "Prev Diagnostic" })
+-- vim.keymap.set("n", "<leader>cj", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
+-- vim.keymap.set("n", "<leader>ck", vim.diagnostic.goto_prev, { desc = "Prev Diagnostic" })
 
 -- Save without autoformatting.
 vim.keymap.set("n", "<leader>bw", "<cmd>noautocmd w<cr>", { desc = "Save without formatting (noautocmd)" })
