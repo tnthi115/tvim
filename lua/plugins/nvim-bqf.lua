5a241f43ed49183714a5cf0af0343c9f49c5f9dd
Tymon Thi <tnthi115@berkeley.edu>
2024-08-12 11:30:56 -0700

    feat(nvim-bqf): add nvim-bqf

---
+++ lua/plugins/nvim-bqf.lua
@@ -0,0 +1,13 @@
+-- https://github.com/kevinhwang91/nvim-bqf
+
+return {
+  {
+    "kevinhwang91/nvim-bqf",
+    ft = { "qf" },
+    opts = {
+      preview = {
+        winblend = 0,
+      },
+    },
+  },
+}
