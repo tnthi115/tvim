e1f651945e68b52aa74f9634f438f23731a55fbc
Tymon Thi <tnthi115@berkeley.edu>
2024-09-26 10:43:21 -0700

    refactor(cmp): add extra action for tab

--- lua/plugins/core/cmp.lua
+++ lua/plugins/core/cmp.lua
@@ -2,6 +2,12 @@
 
 local cmp = require "cmp"
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
 return {
   {
     "hrsh7th/nvim-cmp",
@@ -18,6 +24,8 @@ return {
             vim.fn.feedkey("<C-n>", "n")
           elseif cmp.visible() then
             cmp.select_next_item()
+          elseif has_words_before() then
+            cmp.complete()
           else
             fallback()
           end
