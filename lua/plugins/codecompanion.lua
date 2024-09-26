f5622de4f8559b797c2a46df0adc2cab44f5f943
Tymon Thi <tnthi115@berkeley.edu>
2024-09-26 10:43:03 -0700

    feat(codecompanion): add deepseek-coder model and use fzf_lua for picker

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -7,7 +7,7 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
-      "nvim-telescope/telescope.nvim", -- Optional
+      -- "nvim-telescope/telescope.nvim", -- Optional
       "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
       {
         "folke/which-key.nvim",
@@ -18,15 +18,52 @@ return {
       },
     },
     keys = {
-      { "<leader>aa", ":CodeCompanion" },
+      { "<leader>aa", ":CodeCompanion", mode = { "n", "x", "v" } },
+      { "<leader>ao", "<cmd>CodeCompanionActions<CR>", desc = "Open Actions" },
       { "<leader>ac", "<cmd>CodeCompanionChat<CR>", desc = "Open New Chat Buffer" },
       { "<leader>au", "<cmd>CodeCompanionToggle<CR>", desc = "Toggle Chat Buffer" },
       { "<leader>aA", "<cmd>CodeCompanionAdd<CR>", desc = "Add Selection to Chat Buffer", mode = { "n", "x", "v" } },
     },
     opts = {
+      adapters = {
+        -- ollama by default uses starcoder2:7b
+        deepseekcoder = function()
+          return require("codecompanion.adapters").extend "ollama",
+            {
+              name = "deepseekcoder",
+              schema = {
+                model = {
+                  default = "deepseek-coder:6.7b",
+                },
+              },
+            }
+        end,
+        -- Change ollama's default model
+        ollama = function()
+          return require("codecompanion.adapters").extend("ollama", {
+            schema = {
+              model = {
+                default = "mistral:7b-instruct",
+              },
+            },
+          })
+        end,
+      },
       strategies = {
         chat = {
           adapter = "ollama",
+          slash_commands = {
+            ["buffer"] = {
+              opts = {
+                provider = "fzf_lua",
+              },
+            },
+            ["file"] = {
+              opts = {
+                provider = "fzf_lua",
+              },
+            },
+          },
         },
         inline = {
           adapter = "ollama",
