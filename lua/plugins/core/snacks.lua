5bc14aef0bf17df60f00995ec6365caa27f6d3a0
Tymon Thi <tnthi115@berkeley.edu>
2025-01-17 14:13:27 -0800

    feat(snacks): add snacks.nvim configuration

---
+++ lua/plugins/core/snacks.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    indent = {
+      indent = {
+        -- only_current = true,
+        -- hl = "Normal",
+      },
+      animate = {
+        enabled = false,
+      },
+      scope = {
+        hl = "Normal",
+      },
+      chunk = {
+        enabled = false,
+      },
+    },
+  },
+}
