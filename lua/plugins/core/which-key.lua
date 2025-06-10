2fa347abf7243e591bbca5a8e5b0e4c379ed2ab7
Tymon Thi <tnthi115@berkeley.edu>
2025-06-09 18:12:25 -0700

    refactor(lazyvim): update LazyExtras keybind

--- lua/plugins/core/which-key.lua
+++ lua/plugins/core/which-key.lua
@@ -13,7 +13,7 @@ return {
     spec = {
       { "<leader>l", group = "lazyvim" },
       { "<leader>ll", "<cmd>Lazy<CR>", desc = "Lazy" },
-      { "<leader>le", "<cmd>LazyExtras<CR>", desc = "LazyExtras" },
+      { "<leader>lx", "<cmd>LazyExtras<CR>", desc = "LazyExtras" },
       { "<leader>lc", lazyvim_util.news.changelog, desc = "Changelog" },
       { "<leader>lr", lazyvim_util.root.info, desc = "Root Info" },
       { "<leader>lM", vim.cmd.messages, desc = "Display messages" },
