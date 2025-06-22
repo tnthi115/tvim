ba0652ac59cea8640a4fe7be0c7101ad3f333bd9
Tymon Thi <tnthi115@berkeley.edu>
2025-06-22 00:58:03 -0700

    feat(gitlab): disable gitlab.nvim and enable gitlab.vim
    These have clashing module names so only one can be installed at once.

--- lua/plugins/gitlab.lua
+++ lua/plugins/gitlab.lua
@@ -3,55 +3,61 @@
 
 return {
   {
+    name = "gitlab-mrs",
     "harrisoncramer/gitlab.nvim",
+    enabled = false,
     dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
       "sindrets/diffview.nvim",
       "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
       "nvim-tree/nvim-web-devicons", -- Recommended but not required. Icons in discussion tree.
-      {
-        "folke/which-key.nvim",
-        opts = function(_, opts)
-          local wk = require "which-key"
-          wk.add { "<leader>m", group = "gitlab" }
-          wk.add { "<leader>mr", group = "reviewers" }
-          wk.add { "<leader>mc", group = "comment" }
-          wk.add { "<leader>ma", group = "assignee" }
-          wk.add { "<leader>ml", group = "labels" }
-        end,
-      },
+      -- {
+      --   "folke/which-key.nvim",
+      --   opts = function(_, opts)
+      --     local wk = require "which-key"
+      --     wk.add { "<leader>m", group = "gitlab" }
+      --     wk.add { "<leader>mr", group = "reviewers" }
+      --     wk.add { "<leader>mc", group = "comment" }
+      --     wk.add { "<leader>ma", group = "assignee" }
+      --     wk.add { "<leader>ml", group = "labels" }
+      --   end,
+      -- },
     },
     build = function()
       require("gitlab.server").build(true)
     end, -- Builds the Go binary
     -- stylua: ignore
     keys = {
-      { "<leader>ms", "<cmd>lua require('gitlab').review()<CR>", desc = "Start Gitlab review" },
-      { "<leader>mS", "<cmd>lua require('gitlab').summary()<CR>", desc = "Summary" },
-      { "<leader>mA", "<cmd>lua require('gitlab').approve()<CR>", desc = "Approve MR" },
-      { "<leader>mR", "<cmd>lua require('gitlab').revoke()<CR>", desc = "Revoke approval" },
-      { "<leader>mc", "<cmd>lua require('gitlab').create_comment()<CR>", desc = "Create comment" },
-      { "<leader>mc", "<cmd>lua require('gitlab').create_multiline_comment()<CR>", desc = "Create multiline comment", mode = "v" },
-      { "<leader>mC", "<cmd>lua require('gitlab').create_comment_suggestion()<CR>", desc = "Create comment suggestion", mode = "v" },
-      { "<leader>mm", "<cmd>lua require('gitlab').move_to_discussion_tree_from_diagnostic()<CR>", desc = "Move to discussion tree from diagnostic" },
-      { "<leader>mn", "<cmd>lua require('gitlab').create_note()<CR>", desc = "Create MR note" },
-      { "<leader>md", "<cmd>lua require('gitlab').toggle_discussions()<CR>", desc = "Toggle MR discussions" },
-      { "<leader>maa", "<cmd>lua require('gitlab').add_assignee()<CR>", desc = "Add MR assignee" },
-      { "<leader>mad", "<cmd>lua require('gitlab').delete_assignee()<CR>", desc = "Delete MR assignee" },
-      { "<leader>mla", "<cmd>lua require('gitlab').add_label()<CR>", desc = "Add MR label" },
-      { "<leader>mld", "<cmd>lua require('gitlab').delete_label()<CR>", desc = "Delete MR label" },
-      { "<leader>mra", "<cmd>lua require('gitlab').add_reviewer()<CR>", desc = "Add MR reviewer" },
-      { "<leader>mrd", "<cmd>lua require('gitlab').delete_reviewer()<CR>", desc = "Delete MR reviewer" },
-      { "<leader>mp", "<cmd>lua require('gitlab').pipeline()<CR>", desc = "Show MR pipeline status" },
-      { "<leader>mo", "<cmd>lua require('gitlab').open_in_browser()<CR>", desc = "Open MR in browser" },
-      { "<leader>mM", "<cmd>lua require('gitlab').merge()<CR>", desc = "Merge MR" },
-      { "<leader>mu", "<cmd>lua require('gitlab').copy_mr_url()<CR>", desc = "Copy MR url" },
-      { "<leader>mb", "<cmd>lua require('gitlab').choose_merge_request()<CR>", desc = "Choose MR for Review" },
-      { "<leader>mO", "<cmd>lua require('gitlab').create_mr()<CR>", desc = "Create MR" },
-      { "<leader>mP", "<cmd>lua require('gitlab').publish_all_drafts()<CR>", desc = "Publish all MR comment drafts" },
-      { "<leader>mD", "<cmd>lua require('gitlab').toggle_draft_mode()<CR>", desc = "Toggle MR comment draft mode" },
-      { "<leader>mq", "<cmd>DiffviewClose<CR>", desc = "Quit Review (Diffview)" },
+      { "gl", "", desc = "gitlab MRs" },
+      { "gla", "", desc = "assignees" },
+      { "gll", "", desc = "labels" },
+      { "glr", "", desc = "reviewers" },
+      -- { "<leader>ms", "<cmd>lua require('gitlab').review()<CR>", desc = "Start Gitlab review" },
+      -- { "<leader>mS", "<cmd>lua require('gitlab').summary()<CR>", desc = "Summary" },
+      -- { "<leader>mA", "<cmd>lua require('gitlab').approve()<CR>", desc = "Approve MR" },
+      -- { "<leader>mR", "<cmd>lua require('gitlab').revoke()<CR>", desc = "Revoke approval" },
+      -- { "<leader>mc", "<cmd>lua require('gitlab').create_comment()<CR>", desc = "Create comment" },
+      -- { "<leader>mc", "<cmd>lua require('gitlab').create_multiline_comment()<CR>", desc = "Create multiline comment", mode = "v" },
+      -- { "<leader>mC", "<cmd>lua require('gitlab').create_comment_suggestion()<CR>", desc = "Create comment suggestion", mode = "v" },
+      -- { "<leader>mm", "<cmd>lua require('gitlab').move_to_discussion_tree_from_diagnostic()<CR>", desc = "Move to discussion tree from diagnostic" },
+      -- { "<leader>mn", "<cmd>lua require('gitlab').create_note()<CR>", desc = "Create MR note" },
+      -- { "<leader>md", "<cmd>lua require('gitlab').toggle_discussions()<CR>", desc = "Toggle MR discussions" },
+      -- { "<leader>maa", "<cmd>lua require('gitlab').add_assignee()<CR>", desc = "Add MR assignee" },
+      -- { "<leader>mad", "<cmd>lua require('gitlab').delete_assignee()<CR>", desc = "Delete MR assignee" },
+      -- { "<leader>mla", "<cmd>lua require('gitlab').add_label()<CR>", desc = "Add MR label" },
+      -- { "<leader>mld", "<cmd>lua require('gitlab').delete_label()<CR>", desc = "Delete MR label" },
+      -- { "<leader>mra", "<cmd>lua require('gitlab').add_reviewer()<CR>", desc = "Add MR reviewer" },
+      -- { "<leader>mrd", "<cmd>lua require('gitlab').delete_reviewer()<CR>", desc = "Delete MR reviewer" },
+      -- { "<leader>mp", "<cmd>lua require('gitlab').pipeline()<CR>", desc = "Show MR pipeline status" },
+      -- { "<leader>mo", "<cmd>lua require('gitlab').open_in_browser()<CR>", desc = "Open MR in browser" },
+      -- { "<leader>mM", "<cmd>lua require('gitlab').merge()<CR>", desc = "Merge MR" },
+      -- { "<leader>mu", "<cmd>lua require('gitlab').copy_mr_url()<CR>", desc = "Copy MR url" },
+      -- { "<leader>mb", "<cmd>lua require('gitlab').choose_merge_request()<CR>", desc = "Choose MR for Review" },
+      -- { "<leader>mO", "<cmd>lua require('gitlab').create_mr()<CR>", desc = "Create MR" },
+      -- { "<leader>mP", "<cmd>lua require('gitlab').publish_all_drafts()<CR>", desc = "Publish all MR comment drafts" },
+      -- { "<leader>mD", "<cmd>lua require('gitlab').toggle_draft_mode()<CR>", desc = "Toggle MR comment draft mode" },
+      -- { "<leader>mq", "<cmd>DiffviewClose<CR>", desc = "Quit Review (Diffview)" },
     },
     opts = {
       reviewer_settings = {
@@ -61,7 +67,7 @@ return {
       },
       keymaps = {
         global = {
-          disable_all = true,
+          disable_all = false,
         },
         discussion_tree = {
           switch_view = "<Tab>",
@@ -82,9 +88,11 @@ return {
   -- but it still works I guess?
   -- this is now a paid feature as part of Duo Pro :(
   {
+    name = "gitlab-code-suggestions",
     "https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git",
+    -- "git@gitlab.com:gitlab-org/editor-extensions/gitlab.vim.git",
     -- commit = "cf304d18ba352e7bf914af978f4c1aab7ffb7e49",
-    lazy = false,
+    -- lazy = false,
     -- enabled = false,
     -- event = { "BufReadPre", "BufNewFile" }, -- Activate when a file is created/opened
     event = { "LspAttach" }, -- Doesn't work. Needs to be loaded earlier...
@@ -102,37 +110,45 @@ return {
       statusline = {
         enabled = false, -- Hook into the builtin statusline to indicate the status of the GitLab Duo Code Suggestions integration
       },
-      -- code_suggestions = {
-      --   -- For the full list of default languages, see the 'auto_filetypes' array in
-      --   -- https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim/-/blob/main/lua/gitlab/config/defaults.lua
-      --   auto_filetypes = {
-      --     "c", -- C
-      --     "cpp", -- C++
-      --     "csharp", -- C#
-      --     "go", -- Golang
-      --     "java", -- Java
-      --     "javascript", -- JavaScript
-      --     "javascriptreact", -- JavaScript React
-      --     "kotlin", -- Kotlin
-      --     "lua",
-      --     "markdown", -- Markdown
-      --     "objective-c", -- Objective-C
-      --     "objective-cpp", -- Objective-C++
-      --     "php", -- PHP
-      --     "python", -- Python
-      --     "ruby", -- Ruby
-      --     "rust", -- Rust
-      --     "scala", -- Scala
-      --     "sql", -- SQL
-      --     "swift", -- Swift
-      --     "terraform", -- Terraform
-      --     "typescript", -- TypeScript
-      --     "typescriptreact", -- TypeScript React
-      --     "sh", -- Shell scripts
-      --     "html", -- HTML
-      --     "css", -- CSS
-      --   },
-      -- },
+      code_suggestions = {
+        -- For the full list of default languages, see the 'auto_filetypes' array in
+        -- https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim/-/blob/main/lua/gitlab/config/defaults.lua
+        auto_filetypes = {
+          "c", -- C
+          "cpp", -- C++
+          "csharp", -- C#
+          "go", -- Golang
+          "java", -- Java
+          "javascript", -- JavaScript
+          "javascriptreact", -- JavaScript React
+          "kotlin", -- Kotlin
+          "lua",
+          "markdown", -- Markdown
+          "objective-c", -- Objective-C
+          "objective-cpp", -- Objective-C++
+          "php", -- PHP
+          "python", -- Python
+          "ruby", -- Ruby
+          "rust", -- Rust
+          "scala", -- Scala
+          "sql", -- SQL
+          "swift", -- Swift
+          "terraform", -- Terraform
+          "typescript", -- TypeScript
+          "typescriptreact", -- TypeScript React
+          "sh", -- Shell scripts
+          "html", -- HTML
+          "css", -- CSS
+        },
+        ghost_text = {
+          enabled = true,
+          toggle_enabled = "<C-h>",
+          -- accept_suggestion = "<C-l>",
+          accept_suggestion = "<Tab>",
+          clear_suggestions = "<C-k>",
+          stream = true,
+        },
+      },
       language_server = {
         workspace_settings = {
           telemetry = {
