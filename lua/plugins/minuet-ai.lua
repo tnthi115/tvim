d9cfcd01abd54c401332d3af43179bfa27f56de6
Tymon Thi <tnthi115@berkeley.edu>
2025-06-22 01:01:01 -0700

    feat(minuet-ai): add more config

--- lua/plugins/minuet-ai.lua
+++ lua/plugins/minuet-ai.lua
@@ -43,29 +43,58 @@ return {
         "milanglacier/minuet-ai.nvim",
         opts = {
           provider = "openai_compatible",
-
           provider_options = {
             openai_compatible = {
-              model = "gpt-4o",
+              -- model = "gpt-4o",
+              model = "gpt-4.1-mini",
+              json = {
+                -- model = "gpt-4o",
+                model = "gpt-4.1-mini",
+                -- max_tokens = 1024,
+              },
               -- system = "see [Prompt] section for the default value",
               -- few_shots = "see [Prompt] section for the default value",
               -- chat_input = "See [Prompt Section for default value]",
-              stream = false,
+              stream = true,
               end_point = "https://f5ai.pd.f5net.com/api/chat/completions",
               api_key = "OPENAI_API_KEY",
               name = "f5gpt",
               optional = {
-                stop = nil,
-                max_tokens = nil,
+                --   stop = nil,
+                -- max_tokens = 256, -- default is nil
+                -- temperature = 0.3,
+                -- top_p = 0.9,
+                -- presence_penalty = 0,
+                -- frequency_penalty = 0.2,
               },
             },
           },
+          -- The request timeout, measured in seconds. When streaming is enabled
+          -- (stream = true), setting a shorter request_timeout allows for faster
+          -- retrieval of completion items, albeit potentially incomplete.
+          -- Conversely, with streaming disabled (stream = false), a timeout
+          -- occurring before the LLM returns results will yield no completion items.
+          -- Default: 3
+          request_timeout = 1.5,
+          -- If completion item has multiple lines, create another completion item
+          -- only containing its first line. This option only has impact for cmp and
+          -- blink. For virtualtext, no single line entry will be added.
+          -- Default: true
+          add_single_line_entry = false,
+          -- The number of completion items encoded as part of the prompt for the
+          -- chat LLM. For FIM model, this is the number of requests to send. It's
+          -- important to note that when 'add_single_line_entry' is set to true, the
+          -- actual number of returned items may exceed this value. Additionally, the
+          -- LLM cannot guarantee the exact number of completion items specified, as
+          -- this parameter serves only as a prompt guideline.
+          -- Default: 3
+          n_completions = 3,
         },
       },
     },
     opts = {
       keymap = {
-        ["<C-space>"] = {
+        ["<C-y>"] = {
           function(cmp)
             cmp.show { providers = { "minuet" } }
           end,
@@ -87,27 +116,29 @@ return {
         nerd_font_variant = "normal",
         kind_icons = kind_icons,
       },
-      completion = {
-        menu = {
-          draw = {
-            columns = {
-              { "label", "label_description", gap = 1 },
-              { "kind_icon", "kind" },
-              { "source_icon" },
-            },
-            components = {
-              source_icon = {
-                -- don't truncate source_icon
-                ellipsis = false,
-                text = function(ctx)
-                  return source_icons[ctx.source_name:lower()] or source_icons.fallback
-                end,
-                highlight = "BlinkCmpSource",
-              },
-            },
-          },
-        },
-      },
+      -- TODO: doesn't work
+      -- completion = {
+      --   menu = {
+      --     draw = {
+      --       columns = {
+      --         { "label", "label_description", gap = 1 },
+      --         { "kind_icon", "kind" },
+      --         -- { "source_icon", "kind", gap = 1 },
+      --         { "source_icon" },
+      --       },
+      --       components = {
+      --         source_icon = {
+      --           -- don't truncate source_icon
+      --           ellipsis = false,
+      --           text = function(ctx)
+      --             return source_icons[ctx.source_name:lower()] or source_icons.fallback
+      --           end,
+      --           highlight = "BlinkCmpSource",
+      --         },
+      --       },
+      --     },
+      --   },
+      -- },
     },
   },
 }
