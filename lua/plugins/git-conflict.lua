9422b9b42b4681d03971d079d9afff8b15c89855
Tymon Thi <tnthi115@berkeley.edu>
2025-07-04 22:43:48 -0700

    feat(git): add git-config.nvim for resolving git conflicts

---
+++ lua/plugins/git-conflict.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "akinsho/git-conflict.nvim",
+    event = "LazyFile",
+    version = "*",
+    command = {
+      "GitConflictChooseOurs",
+      "GitConflictChooseTheirs",
+      "GitConflictChooseBoth",
+      "GitConflictChooseNone",
+      "GitConflictNextConflict",
+      "GitConflictPrevConflict",
+      "GitConflictListQf",
+    },
+    config = function()
+      require("git-conflict").setup {}
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "GitConflictDetected",
+        callback = function()
+          vim.notify("Conflict detected in " .. vim.fn.expand "<afile>")
+          vim.keymap.set("n", "cww", function()
+            engage.conflict_buster()
+            create_buffer_local_mappings()
+          end)
+        end,
+      })
+    end,
+  },
+}
