63931e7da4c63c3aba0e76ceea0cbdac54a166b9
Tymon Thi <tnthi115@berkeley.edu>
2025-07-04 22:44:43 -0700

    feat(dropbar): add modified symbol to filename

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -6,14 +6,31 @@ return {
     -- optional, but required for fuzzy finder support
     event = "LazyFile",
     dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
-    opts = function()
-      -- vim.api.nvim_set_hl(0, "DropBarIconUISeparator", { link = "Comment" })
-      -- vim.api.nvim_set_hl(0, "DropBarPreview", { link = "Normal" })
-      -- vim.api.nvim_set_hl(0, "DropBarCurrentContext", { link = "Normal" })
-      -- vim.api.nvim_set_hl(0, "BufferLineBackground", { link = "Normal" })
-      vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE" }) -- no background for dropbar
-    end,
+    opts = {
+      sources = {
+        path = {
+          modified = function(sym)
+            return sym:merge {
+              -- name = sym.name .. "[+]",
+              name = sym.name .. " ● ",
+              -- icon = " ",
+              name_hl = "DiffAdded",
+              -- name_hl = "DiffChanged",
+              -- icon_hl = "DiffChanged",
+              -- ...
+            }
+          end,
+        },
+      },
+    },
+    -- opts = function()
+    --   -- vim.api.nvim_set_hl(0, "DropBarIconUISeparator", { link = "Comment" })
+    --   -- vim.api.nvim_set_hl(0, "DropBarPreview", { link = "Normal" })
+    --   -- vim.api.nvim_set_hl(0, "DropBarCurrentContext", { link = "Normal" })
+    --   -- vim.api.nvim_set_hl(0, "BufferLineBackground", { link = "Normal" })
+    --   -- vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE" }) -- no background for dropbar
+    -- end,
   },
 }
