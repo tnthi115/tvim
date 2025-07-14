16f0474550f76d50c3ac362377a8590134dbd99d
t.thi <tnthi115@berkeley.edu>
2025-07-14 11:19:40 -0700

    refactor(gitlab): separate gitlab.nvim and gitlab.vim

---
+++ lua/plugins/gitlab-duo-code-suggestions.lua
@@ -0,0 +1,82 @@
+-- https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim
+-- https://docs.gitlab.com/editor_extensions/neovim/setup/?tab=%60lazy.nvim%60
+
+-- https://docs.gitlab.com/user/project/repository/code_suggestions/supported_extensions/#supported-languages-by-ide
+local supported_languages = {
+  "c", -- C
+  "cpp", -- C++
+  "csharp", -- C#
+  "css", -- CSS (not supported in neovim)
+  "go", -- Golang
+  "haml",
+  "html", -- HTML (not supported in neovim)
+  "java", -- Java
+  "javascript", -- JavaScript
+  "javascriptreact", -- JavaScript React
+  "kotlin", -- Kotlin
+  "lua", -- Lua (not supported)
+  "markdown", -- Markdown (not supported in neovim)
+  "objective-c", -- Objective-C
+  "objective-cpp", -- Objective-C++
+  "php", -- PHP
+  "python", -- Python
+  "ruby", -- Ruby
+  "rust", -- Rust
+  "scala", -- Scala
+  "sh", -- Shell scripts (bash only)
+  "bash",
+  "svelte", -- Svelte
+  "sql", -- Google SQL
+  "swift", -- Swift
+  "terraform", -- Terraform
+  "typescript", -- TypeScript
+  "typescriptreact", -- TypeScript React
+  "vue", -- Vue.js
+}
+
+return {
+  {
+    name = "gitlab-duo-code-suggestions",
+    "https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git",
+    -- "git@gitlab.com:gitlab-org/editor-extensions/gitlab.vim.git",
+    -- enabled = false,
+    -- event = { "BufReadPre", "BufNewFile" }, -- Activate when a file is created/opened
+    -- event = { "LspAttach" },
+    -- event = { "LazyFile" },
+    ft = supported_languages, -- Activate when a supported filetype is open. For supported languages, see https://docs.gitlab.com/ee/user/project/repository/code_suggestions/index.html#supported-languages
+    cond = function()
+      -- Only activate is token is present in environment variable (remove to use interactive workflow)
+      -- return vim.env.GITLAB_DUO_AI_TOKEN ~= nil and vim.env.GITLAB_DUO_AI_TOKEN ~= ""
+      return vim.env.GITLAB_TOKEN ~= nil and vim.env.GITLAB_TOKEN ~= ""
+    end,
+    keys = {
+      { "<leader>cu", "", desc = "ui/toggles" },
+      { "<leader>cug", "<Plug>(GitLabToggleCodeSuggestions)", desc = "Toggle Gitlab Duo Code Suggestions" },
+    },
+    opts = {
+      statusline = {
+        enabled = false, -- Hook into the builtin statusline to indicate the status of the GitLab Duo Code Suggestions integration
+      },
+      code_suggestions = {
+        -- For the full list of default languages, see the 'auto_filetypes' array in
+        -- https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim/-/blob/main/lua/gitlab/config/defaults.lua
+        auto_filetypes = supported_languages,
+        ghost_text = {
+          enabled = true,
+          toggle_enabled = "<C-h>",
+          -- accept_suggestion = "<C-l>",
+          accept_suggestion = "<Tab>",
+          clear_suggestions = "<C-k>",
+          stream = true,
+        },
+      },
+      language_server = {
+        workspace_settings = {
+          telemetry = {
+            enabled = false,
+          },
+        },
+      },
+    },
+  },
+}
--- lua/plugins/gitlab-mrs.lua
+++ lua/plugins/gitlab-mrs.lua
@@ -1,5 +1,9 @@
 -- https://github.com/harrisoncramer/gitlab.nvim
--- gitlab integration
+-- gitlab MR integration
+
+if true then
+  return {}
+end
 
 return {
   {
@@ -79,113 +83,5 @@ return {
         position = "bottom",
       },
     },
-  },
-
-  -- https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim
-  -- gitlab duo code suggestions
-  -- for some reason I get a warning when there are other language servers active (e.g. doesn't work when pyright is active):
-  --   warning: multiple different client offset_encodings detected for buffer, this is not supported yet
-  -- but it still works I guess?
-  -- this is now a paid feature as part of Duo Pro :(
-  {
-    name = "gitlab-code-suggestions",
-    "https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git",
-    -- "git@gitlab.com:gitlab-org/editor-extensions/gitlab.vim.git",
-    -- commit = "cf304d18ba352e7bf914af978f4c1aab7ffb7e49",
-    -- lazy = false,
-    -- enabled = false,
-    -- event = { "BufReadPre", "BufNewFile" }, -- Activate when a file is created/opened
-    event = { "LspAttach" }, -- Doesn't work. Needs to be loaded earlier...
-    -- event = { "LazyFile" },
-    ft = { "go", "python" }, -- Activate when a supported filetype is open. For supported languages, see https://docs.gitlab.com/ee/user/project/repository/code_suggestions/index.html#supported-languages
-    cond = function()
-      -- Only activate is token is present in environment variable (remove to use interactive workflow)
-      -- return vim.env.GITLAB_DUO_AI_TOKEN ~= nil and vim.env.GITLAB_DUO_AI_TOKEN ~= ""
-      return vim.env.GITLAB_TOKEN ~= nil and vim.env.GITLAB_TOKEN ~= ""
-    end,
-    keys = {
-      { "<leader>u.", "<Plug>(GitLabToggleCodeSuggestions)", desc = "Toggle Gitlab Duo Code Suggestions" },
-    },
-    opts = {
-      statusline = {
-        enabled = false, -- Hook into the builtin statusline to indicate the status of the GitLab Duo Code Suggestions integration
-      },
-      code_suggestions = {
-        -- For the full list of default languages, see the 'auto_filetypes' array in
-        -- https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim/-/blob/main/lua/gitlab/config/defaults.lua
-        auto_filetypes = {
-          "c", -- C
-          "cpp", -- C++
-          "csharp", -- C#
-          "go", -- Golang
-          "java", -- Java
-          "javascript", -- JavaScript
-          "javascriptreact", -- JavaScript React
-          "kotlin", -- Kotlin
-          "lua",
-          "markdown", -- Markdown
-          "objective-c", -- Objective-C
-          "objective-cpp", -- Objective-C++
-          "php", -- PHP
-          "python", -- Python
-          "ruby", -- Ruby
-          "rust", -- Rust
-          "scala", -- Scala
-          "sql", -- SQL
-          "swift", -- Swift
-          "terraform", -- Terraform
-          "typescript", -- TypeScript
-          "typescriptreact", -- TypeScript React
-          "sh", -- Shell scripts
-          "html", -- HTML
-          "css", -- CSS
-        },
-        ghost_text = {
-          enabled = true,
-          toggle_enabled = "<C-h>",
-          -- accept_suggestion = "<C-l>",
-          accept_suggestion = "<Tab>",
-          clear_suggestions = "<C-k>",
-          stream = true,
-        },
-      },
-      language_server = {
-        workspace_settings = {
-          telemetry = {
-            enabled = false,
-          },
-        },
-      },
-    },
-    --   },
-    --   config = function()
-    --     require("gitlab").setup()
-    --
-    --     -- TODO: Make this work. It currently disables/overrides nvim-notify.
-    --
-    --     -- local notify = vim.notify
-    --     -- vim.notify = function(msg, ...)
-    --     --   if msg:match "warning: multiple different client offset_encodings" then
-    --     --     return
-    --     --   end
-    --     --
-    --     --   local nvim_notify_loaded, nvim_notify = pcall(require, "notify")
-    --     --   if not nvim_notify_loaded then
-    --     --     notify(msg, ...)
-    --     --   else
-    --     --     nvim_notify(msg, ...)
-    --     --   end
-    --     -- end
-    --     -- local banned_messages = { "warning: multiple different client offset_encodings" }
-    --     --
-    --     -- vim.notify = function(msg, ...)
-    --     --   for _, banned in ipairs(banned_messages) do
-    --     --     if msg == banned then
-    --     --       return
-    --     --     end
-    --     --   end
-    --     --   require "notify"(msg, ...)
-    --     -- end
-    --   end,
   },
 }
