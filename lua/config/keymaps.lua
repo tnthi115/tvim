7b66c1a11284c940f513f5a715d3fe7c487533b2
Tymon Thi <tnthi115@berkeley.edu>
2025-07-30 00:33:26 -0700

    feat(keymaps): add <leader>bk to delete buffers, like <leader>bd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -78,6 +78,10 @@ end
 -- vim.keymap.set("n", "<leader>bP", "<cmd>echo expand('%:p)')<CR>", { desc = "Show Buffer Path" })
 vim.keymap.set("n", "<leader>bP", echo_and_copy_buffer_path, { desc = "Show Buffer Path" })
 
+vim.keymap.set("n", "<leader>bk", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
+
 -- These are now in LazyVim with [B and ]B
 -- vim.keymap.set("n", "<leader>b,", "<cmd>BufferLineMovePrev<CR>", { desc = "Move Buffer Left" })
 -- vim.keymap.set("n", "<leader>b.", "<cmd>BufferLineMoveNext<CR>", { desc = "Move Buffer Right" })
