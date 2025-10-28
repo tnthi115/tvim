13ac00b4126985f6c3a2c6ec090ffc21e70d8252
Tymon Thi <tnthi115@berkeley.edu>
2025-10-28 15:43:38 -0700

    chore(lang/gitlab-ci): relocate and disable GitLab CI validation spec
    Remove the validate-gitlab-ci plugin spec from lua/plugins/core/linting.lua
    and place a disabled (enabled = false) entry in lua/plugins/lang/gitlab-ci.lua.
    Add an early return to short-circuit the gitlab-ci spec while keeping the
    configuration for later. Also simplify the nvim-lspconfig setup by assigning
    opts.servers.gitlab_ci_ls = {} instead of using vim.list_extend.

--- lua/plugins/lang/gitlab-ci.lua
+++ lua/plugins/lang/gitlab-ci.lua
@@ -1,9 +1,30 @@
--- https://github.com/alesbrelih/gitlab-ci-ls
-
--- TODO: figure this out, not working as expected
+if true then
+  return {}
+end
 
 return {
+  -- https://github.com/sbulav/validate-gitlab-ci.nvim
+  {
+    "sbulav/validate-gitlab-ci.nvim",
+    -- "tnthi115/validate-gitlab-ci.nvim", -- use my fork with the fix for now
+    enabled = false,
+    event = "BufEnter .gitlab-ci.yml",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      vim.api.nvim_create_augroup("ValidateGitlabCIfiles", { clear = true })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        callback = function()
+          require("validate-gitlab-ci.validate-gitlab-ci").validate()
+        end,
+        group = "ValidateGitlabCIfiles",
+        desc = "Validate Gitlab CI  files on save",
+        pattern = ".gitlab-ci.yml",
+      })
+    end,
+  },
   -- Setup gitlab_ci_ls.
+  -- https://github.com/alesbrelih/gitlab-ci-ls
+  -- TODO: figure this out, not working as expected
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
@@ -12,9 +33,7 @@ return {
           ["%.gitlab%-ci%.ya?ml"] = "yaml.gitlab",
         },
       }
-      vim.list_extend(opts.servers, {
-        gitlab_ci_ls = {},
-      })
+      opts.servers.gitlab_ci_ls = {}
     end,
   },
 }
